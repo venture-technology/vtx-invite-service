65422c4f33a8dd90a81220602853fe4723f6397d
gurodrigues-dev <gustavorodrigueslima2004@gmail.com>
2024-06-18 08:58:46 -0300

    feat: driver interface to uses repository

--- internal/repository/invite_repository.go
+++ internal/repository/invite_repository.go
@@ -1,1 +1,16 @@
 package repository
+
+import (
+	"context"
+
+	"github.com/venture-technology/vtx-invites/models"
+)
+
+type IDriverRepository interface {
+	IsEmployee(ctx context.Context, cnh *string) error
+	CreateInvite(ctx context.Context, invite *models.Invite) error
+	ReadInvite(ctx context.Context, invite_id *int) (*models.Invite, error)
+	ReadAllInvites(ctx context.Context, cnh *string) ([]models.Invite, error)
+	UpdateInvite(ctx context.Context, invite_id *int) error
+	DeleteInvite(ctx context.Context, invite_id *int) error
+}
