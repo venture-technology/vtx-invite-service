c02d0d132be81b6d3887ec53bec39c1b90060012
gurodrigues-dev <gustavorodrigueslima2004@gmail.com>
2024-06-18 08:57:43 -0300

    feat: invite tables

---
+++ database/schema.sql
@@ -0,0 +1,13 @@
+-- Tabela de Convites
+CREATE TABLE IF NOT EXISTS invites (
+    invite_id SERIAL PRIMARY KEY,
+    requester VARCHAR(14), -- school
+    school VARCHAR(100) NOT NULL, --name_school
+    email_school VARCHAR(100) NOT NULL,
+    guest VARCHAR(14), -- driver
+    driver VARCHAR(100) NOT NULL, --name_driver
+    email_driver VARCHAR(100) NOT NULL,
+    status TEXT NOT NULL,
+    FOREIGN KEY (requester) REFERENCES schools(cnpj),
+    FOREIGN KEY (guest) REFERENCES drivers(cnh)
+);
