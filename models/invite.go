100dbba031c10254343cb18debfb5303f69d91c5
gurodrigues-dev <gustavorodrigueslima2004@gmail.com>
2024-06-18 08:58:10 -0300

    feat: models invite

--- models/invite.go
+++ models/invite.go
@@ -1,1 +1,14 @@
 package models
+
+type Driver struct {
+}
+
+type School struct {
+}
+
+type Invite struct {
+	ID     int    `json:"id"`
+	School School `json:"school"`
+	Driver Driver `json:"driver"`
+	Status string `json:"status"`
+}
