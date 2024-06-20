cfdbeebc1882625e70bc0274c647f26b990d658b
gurodrigues-dev <gustavorodrigueslima2004@gmail.com>
2024-06-19 22:25:59 -0300

    feat: services calling repo

--- internal/service/invite_service.go
+++ internal/service/invite_service.go
@@ -1,1 +1,44 @@
 package service
+
+import (
+	"context"
+
+	"github.com/venture-technology/vtx-invites/internal/repository"
+	"github.com/venture-technology/vtx-invites/models"
+)
+
+type InviteService struct {
+	inviterepository repository.IInviteRepository
+	kafkarepository  repository.IKafkaRepository
+}
+
+func NewInviteService(repo repository.IInviteRepository, kafkarepo repository.IKafkaRepository) *InviteService {
+	return &InviteService{
+		inviterepository: repo,
+		kafkarepository:  kafkarepo,
+	}
+}
+
+func (i *InviteService) CreateInvite(ctx context.Context, invite *models.Invite) error {
+	return i.inviterepository.CreateInvite(ctx, invite)
+}
+
+func (i *InviteService) ReadInvite(ctx context.Context, invite_id *int) (*models.Invite, error) {
+	return i.inviterepository.ReadInvite(ctx, invite_id)
+}
+
+func (i *InviteService) ReadAllInvites(ctx context.Context, cnh *string) ([]models.Invite, error) {
+	return i.inviterepository.ReadAllInvites(ctx, cnh)
+}
+
+func (i *InviteService) AcceptedInvite(ctx context.Context, invite_id *int) error {
+	return i.inviterepository.AcceptedInvite(ctx, invite_id)
+}
+
+func (i *InviteService) DeclineInvite(ctx context.Context, invite_id *int) error {
+	return i.inviterepository.DeclineInvite(ctx, invite_id)
+}
+
+func (i *InviteService) IsEmployee(ctx context.Context, cnh *string) error {
+	return i.inviterepository.IsEmployee(ctx, cnh)
+}
